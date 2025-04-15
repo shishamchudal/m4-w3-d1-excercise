3415af1221cfcbc865cf4d90021d6a7809d98c82
Shisham Chudal <shishamchudal61@gmail.com>
2025-04-15 14:58:37 -0700

    Add snapshot test for MyComponent initial layout

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -6,4 +6,8 @@ describe("MyComponent", () => {
   it("should render correctly", () => {
     const component = shallow(<MyComponent />);
   });
+  it("should render initial layout", () => {
+    const component = shallow(<MyComponent />);
+    expect(component.getElements()).toMatchSnapshot();
+  });
 });
